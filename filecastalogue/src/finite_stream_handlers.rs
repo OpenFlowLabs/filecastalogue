bd57163b3e653ffa6f0de56f46a389fd4a36d28e
Aziroshin <aziroshin@gmx.net>
2021-06-22 16:53:21 +0200

    🚧 Basic FiniteStreamHandler implementation.

--- filecastalogue/src/finite_stream_handlers.rs
+++ filecastalogue/src/finite_stream_handlers.rs
@@ -1,6 +1,8 @@
-use std::{fs::File, io::{self, BufReader, BufWriter}, path::{Path, PathBuf}};
+use std::{fs::File, io::{BufReader, BufWriter}, path::{Path, PathBuf}};
 
 use serde::{Serialize, de::{DeserializeOwned}};
+
+use crate::error::FcResult;
 
 pub struct LocalFile {
     path: PathBuf
@@ -15,21 +17,25 @@ impl LocalFile {
 }
 
 pub trait FiniteStreamHandler {
-    fn read_all<Target>(self: &mut Self) -> Result<Target, io::Error>
+    fn new<PathRef: AsRef<Path>>(path: PathRef) -> Self;
+    fn read_all<Target>(self: &mut Self) -> FcResult<Target>
     where Target: DeserializeOwned;
-    fn write_all<Source>(self: &mut Self, source: &Source) -> Result<(), io::Error>
+    fn write_all<Source>(self: &mut Self, source: &Source) -> FcResult<()>
     where Source: ?Sized + Serialize;
 }
 
 impl FiniteStreamHandler for LocalFile {
-    fn read_all<Target>(self: &mut Self) -> Result<Target, io::Error>
+    fn new<PathRef: AsRef<Path>>(path: PathRef) -> Self {
+        Self::new(path)
+    }
+    fn read_all<Target>(self: &mut Self) -> FcResult<Target>
     where Target: DeserializeOwned {
         Ok(serde_json::from_reader(
             BufReader::new(File::open(self.path.to_owned())?)
         )?)
     }
 
-    fn write_all<Source>(self: &mut Self, source: &Source) -> Result<(), io::Error>
+    fn write_all<Source>(self: &mut Self, source: &Source) -> FcResult<()>
     where Source: ?Sized + Serialize {
         Ok(serde_json::to_writer_pretty(
             BufWriter::new(File::open(self.path.to_owned())?),
