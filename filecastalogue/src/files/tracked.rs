ab37a0659bc24a429fcd5dd15a2ad0c21661845f
Aziroshin <aziroshin@gmx.net>
2021-07-29 20:46:30 +0200

    ✨ Implementation for TrackedFile and TrackedFileCollection.

--- filecastalogue/src/files/tracked.rs
+++ filecastalogue/src/files/tracked.rs
@@ -1,23 +1,66 @@
-use super::{RepoFile, blob::BlobProvider};
+use std::{convert::{TryInto}, io::{Read, Write}};
+use crate::{error::{FcResult}, meta::blob::model::Blob};
+use super::{RepoFile, blob::BlobProvider, hashable::Hashable};
 
 /* Notes:
-    There are two reasons why this exists:
-    - Consistency with the "Provider" pattern of other RepoFiles.
-      The rest of the world doesn't care that Tracked files are special
-      as in that their canonical content is simply their binary blob.
-    - It decouples the concern of "Tracked file provides things" from
-      "blob files have blobs".
+        There are two reasons why this exists:
+        - Consistency with the "Provider" pattern of other RepoFiles.
+            The rest of the world doesn't care that Tracked files are special
+            as in that their canonical content is simply their binary blob.
+        - It decouples the concern of "Tracked file provides things" from
+            "blob files have blobs".
 */
 /// Intended for access to the actual binary blob of a Tracked file.
 pub trait TrackedBlobProvider: BlobProvider {}
 
 /* Notes:
-    Technically, this should probably be named "RepoTrackedBlobFile", with
-    everything that's just named "Tracked" getting renamed to "TrackedBlob"
-    However, index files'd be affected by that logic too. Currently, "Blob"
-    is being dropped in the naming for the sake of brevity.
+        Technically, this should probably be named "RepoTrackedBlobFile", with
+        everything that's just named "Tracked" getting renamed to "TrackedBlob"
+        However, index files'd be affected by that logic too. Currently, "Blob"
+        is being dropped in the naming for the sake of brevity.
 */
 /// This is the codified representation of a Tracked blob file, a file
 /// that's stored in the Repo with the purpose of holding the binary blob
 /// of a file we're tracking.
-pub trait RepoTrackedFile: RepoFile + TrackedBlobProvider {}
+pub trait RepoTrackedFile: RepoFile + TrackedBlobProvider
++ BlobProvider+ Hashable {}
+
+pub struct TrackedFile {
+    pub blob: Blob
+}
+
+impl TrackedFile {
+    pub fn from_existing(readable: &mut (dyn Read)) -> FcResult<Self> {
+        Ok(Self {
+            blob: readable.try_into()?
+        })
+    }
+}
+
+impl RepoFile for TrackedFile {
+    fn load(self: &mut Self, readable: &mut (dyn Read)) -> FcResult<()> {
+        self.blob = readable.try_into()?;
+        Ok(())
+    }
+
+    fn save(self: &mut Self, writeable: &mut (dyn Write))-> FcResult<()> {
+        writeable.write(&self.blob)?;
+        Ok(())
+    }
+}
+
+impl BlobProvider for TrackedFile {
+    fn get_blob(&self) -> FcResult<Blob> {
+        Ok(self.blob.clone())
+    }
+}
+
+impl Hashable for TrackedFile {
+    fn get_hash(&self) -> FcResult<String> {
+        (self as &(dyn BlobProvider)).get_hash()
+    }
+}
+
+impl TrackedBlobProvider for TrackedFile {}
+
+impl RepoTrackedFile for TrackedFile {}
--- filecastalogue/src/files/tracked_collection.rs
+++ filecastalogue/src/files/tracked_collection.rs
@@ -1,5 +1,16 @@
-use crate::opaque_collection_handlers::OpaqueCollectionHandler;
-pub trait TrackedFileCollection {}
+use std::ffi::{OsStr, OsString};
+use crate::{error::FcResult,
+    opaque_collection_handlers::OpaqueCollectionHandler};
+use super::tracked::{RepoTrackedFile, TrackedFile};
+
+pub trait TrackedFileCollection {
+    fn has_file(self: &mut Self, hash: &str) -> FcResult<bool>;
+    fn get_file(self: &mut Self, hash: &str)
+    -> FcResult<Box<dyn RepoTrackedFile>>;
+    fn put_file(
+        self: &mut Self, tracked_file: &mut (dyn RepoTrackedFile))
+    -> FcResult<String>;
+}
 
 pub struct MiscTrackedFileCollection<Handler>
 where Handler: OpaqueCollectionHandler<> {
@@ -14,4 +25,32 @@ impl<Handler: OpaqueCollectionHandler> MiscTrackedFileCollection<Handler> {
     }
 }
 
-impl<Handler: OpaqueCollectionHandler> TrackedFileCollection for MiscTrackedFileCollection<Handler> {}
+impl<Handler: OpaqueCollectionHandler> TrackedFileCollection
+for MiscTrackedFileCollection<Handler> {
+    fn has_file(self: &mut Self, hash: &str) -> FcResult<bool> {
+        self.handler.has_file(hash)
+    }
+
+    fn get_file(self: &mut Self, hash: &str)
+    -> FcResult<Box<dyn RepoTrackedFile>> {
+        let mut readable = self.handler.get_file_readable(
+            OsStr::new(hash)
+        )?;
+        let tracked_file: Box<dyn RepoTrackedFile> = Box::new(
+            TrackedFile::from_existing(
+                &mut readable
+            )?
+        );
+        Ok(tracked_file)
+    }
+
+    fn put_file(
+        self: &mut Self, tracked_file: &mut (dyn RepoTrackedFile))
+    -> FcResult<String> {
+        let hash = tracked_file.get_hash()?;
+        let mut writeable = self.handler.get_file_writeable(
+            &OsString::from(&hash))?;
+        tracked_file.save(&mut writeable)?;
+        Ok(hash)
+    }
+}
