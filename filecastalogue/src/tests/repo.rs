b612af0595c61078aa15656e6e591cc0ac690a5b
Aziroshin <aziroshin@gmx.net>
2021-09-19 15:12:28 +0200

    🧪 Failing test added: `add_version_succeeds`.

--- filecastalogue/src/tests/repo.rs
+++ filecastalogue/src/tests/repo.rs
@@ -1,9 +1,28 @@
-use crate::{error::FcResult, tests::test_fixtures::repo::NON_EXISTENT_VERSION_ID,
-tests::test_fixtures};
+use crate::{
+    error::FcResult,
+    meta::{
+        version::model::Version
+    },
+    tests::test_fixtures::repo::NON_EXISTENT_VERSION_ID,
+    tests::test_fixtures
+};
 
 #[test]
 fn has_version_returns_false_when_repo_does_not_have_version() -> FcResult<()> {
     let mut repo = test_fixtures::repo::create_minimal_repo_struct()?;
     assert_eq!(repo.has_version(NON_EXISTENT_VERSION_ID)?, false);
+    Ok(())
+}
+
+/// Happy path testing of `Repo::add_version`.
+#[test]
+fn add_version_succeeds() -> FcResult<()> {
+    let version_id = "added_version";
+    let version = Version::new();
+    
+    let mut repo = test_fixtures::repo::create_minimal_repo_struct()?;
+    repo.add_version(version_id, version)?;
+    
+    assert_eq!(repo.has_version(version_id)?, true);
     Ok(())
 }
