a3ffa5a01bb256230a5b6396bacb475de44c83a8
Aziroshin <aziroshin@gmx.net>
2022-08-22 10:24:42 +0200

    ⚗️ Dabbling with a `os_string_path` feature config (using lossy dummy business logic).

--- filecastalogue/src/meta/index/model.rs
+++ filecastalogue/src/meta/index/model.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, ffi::OsString};
+use std::{collections::HashMap, ffi::{OsString, OsStr}};
 use serde::{Serialize, Deserialize};
 use super::super::file_aspects::enums::TrackedFileAspects;
 
@@ -13,4 +13,18 @@ impl Index {
             files: HashMap::new()
         }
     }
+
+    #[cfg(not(feature = "os_string_path"))]
+    pub fn string_from_os_string(os_string: &OsStr) -> String {
+        // Just testing. Using a lossy conversion would
+        // defeat the entire purpose of using OsString paths.
+        os_string.to_string_lossy().to_string()
+    }
+
+    #[cfg(feature = "os_string_path")]
+    pub fn string_from_os_string(os_string: &OsStr) -> String {
+        // TODO ]dummy-cleanup]: Proper implementation.
+        String::new()
+    }
+
 }
