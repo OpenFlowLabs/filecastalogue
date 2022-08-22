cf2d9b67f0702dc9e6d0241772aee31bcff0fde0
Aziroshin <aziroshin@gmx.net>
2022-08-22 11:59:00 +0200

    🚧 Boilerplate for custom OsString serializer.

--- filecastalogue/src/meta/index/model.rs
+++ filecastalogue/src/meta/index/model.rs
@@ -1,10 +1,36 @@
 use std::{collections::HashMap, ffi::{OsString, OsStr}};
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serializer, Serialize, ser::SerializeMap};
 use super::super::file_aspects::enums::TrackedFileAspects;
 
-#[derive(Serialize, Deserialize, Debug, Eq, PartialEq, Clone)]
+#[derive(Deserialize, Debug, Eq, PartialEq, Clone)]
 pub struct Index {
     pub files: HashMap<OsString, TrackedFileAspects>
+}
+
+impl Serialize for Index {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer {
+        let mut serialized = serializer.serialize_map(Some(self.files.len()))?;
+        
+        // Naming philosophy:
+        //   `k_` for key and `v_` for value as prefixes, followed
+        //   by a descriptive name. This seems useful, since the
+        //   struct we're implementing something for (`Index`)
+        //   is non-generic in nature and is tightly wound around very
+        //   specific domain items (a "path" key and a certain business
+        //   logic related data structure).
+        for (k_path, v_aspects) in &self.files {
+            // TODO: Investigate `serialize_entry`'s behaviour for
+            // non-unicode OsString keys. This is also a good point
+            // to look into non-unicode related improvements and
+            // error handling (at least as far as serialization is 
+            // concerned). Right now, this comes with a high potential
+            // for confusing malfunctions.
+            serialized.serialize_entry(&k_path, &v_aspects)?;
+        }
+        serialized.end()
+    }
 }
 
 impl Index {
