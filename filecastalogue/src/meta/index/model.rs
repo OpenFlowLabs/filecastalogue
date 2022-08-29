c8cedf0581f6433ef74f4079a45bb20d0813d66b
Aziroshin <aziroshin@gmx.net>
2022-08-29 15:20:56 +0200

    🎨 Moved impl directly below the struct definition.

--- filecastalogue/src/meta/index/model.rs
+++ filecastalogue/src/meta/index/model.rs
@@ -7,6 +7,27 @@ pub struct Index {
     pub files: HashMap<OsString, TrackedFileAspects>
 }
 
+impl Index {
+    pub fn new() -> Self {
+        Self {
+            files: HashMap::new()
+        }
+    }
+
+    #[cfg(not(feature = "os_string_path"))]
+    pub fn string_from_os_string(os_string: &OsStr) -> String {
+        // Just testing. Using a lossy conversion would
+        // defeat the entire purpose of using OsString paths.
+        os_string.to_string_lossy().to_string()
+    }
+
+    #[cfg(feature = "os_string_path")]
+    pub fn string_from_os_string(os_string: &OsStr) -> String {
+        // TODO ]dummy-cleanup]: Proper implementation.
+        String::new()
+    }
+}
+
 impl Serialize for Index {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
     where
@@ -31,26 +52,4 @@ impl Serialize for Index {
         }
         serialized.end()
     }
-}
-
-impl Index {
-    pub fn new() -> Self {
-        Self {
-            files: HashMap::new()
-        }
-    }
-
-    #[cfg(not(feature = "os_string_path"))]
-    pub fn string_from_os_string(os_string: &OsStr) -> String {
-        // Just testing. Using a lossy conversion would
-        // defeat the entire purpose of using OsString paths.
-        os_string.to_string_lossy().to_string()
-    }
-
-    #[cfg(feature = "os_string_path")]
-    pub fn string_from_os_string(os_string: &OsStr) -> String {
-        // TODO ]dummy-cleanup]: Proper implementation.
-        String::new()
-    }
-
 }
