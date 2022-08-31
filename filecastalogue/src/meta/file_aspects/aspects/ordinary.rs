1812de3ba527f4c812ac2eb1bafaa438a8234da7
Aziroshin <aziroshin@gmx.net>
2022-08-31 15:46:26 +0200

    🎨 Fixed improper indent of comment text.

--- filecastalogue/src/meta/file_aspects/aspects/ordinary.rs
+++ filecastalogue/src/meta/file_aspects/aspects/ordinary.rs
@@ -4,23 +4,23 @@ use crate::meta::blob::repo_exported::RepoExportedOrdinaryBlobProvider;
 use super::super::attributes::Attributes;
 
 /* Notes:
-     Leaving the .hash out for the Trackable model enables a separation of
-     concern regarding the hash: The Repo needs the hash and will need all
-     the pieces to obtain it (e.g. the file's binary data - which it'll
-     need anyway in order to actually track anything useful in the case of
-     an ordinary file, regardless of its hash needs), however, other actors
-     in the system might be completely oblivious not just to the hash, but
-     even the file's binary data itself - which, hypothetically, might arrive
-     through a completely different channel (e.g. a dedicated file server)
-     than the aspects (e.g. some JSON from some other tool).
+    Leaving the .hash out for the Trackable model enables a separation of
+    concern regarding the hash: The Repo needs the hash and will need all
+    the pieces to obtain it (e.g. the file's binary data - which it'll
+    need anyway in order to actually track anything useful in the case of
+    an ordinary file, regardless of its hash needs), however, other actors
+    in the system might be completely oblivious not just to the hash, but
+    even the file's binary data itself - which, hypothetically, might arrive
+    through a completely different channel (e.g. a dedicated file server)
+    than the aspects (e.g. some JSON from some other tool).
 
-     A lot of future flexibility is added by uncoupling this concern.
+    A lot of future flexibility is added by uncoupling this concern.
 
-     It also enables a cleaner library interface free of this potentially
-     irrelevant concern for its consumers, tighter scoping of the
-     hashing related code bits whilst still providing a means of explicitly
-     codifying a model for file aspects in procedures outside the scope
-     of the repo.
+    It also enables a cleaner library interface free of this potentially
+    irrelevant concern for its consumers, tighter scoping of the
+    hashing related code bits whilst still providing a means of explicitly
+    codifying a model for file aspects in procedures outside the scope
+    of the repo.
 */
 /// Aspects of an ordinary file relevant when not tracked in a Repo, which
 /// for example, is the case when an actor is preparing data for consumption
